0d8b95b00ae0eae9b7385a7665ab192f2ca59b00
Daniel App <32045098+danbapp@users.noreply.github.com>
2021-10-17 13:11:21 -0400

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+  return customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 1;
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 2;
+}
